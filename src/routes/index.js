5f8f7e5aa082d9337d79b1570d460dfcd40ace7d
Felipe <felipe.labarca.c@gmail.com>
2023-04-25 17:32:04 -0400

    Final commit de la estructura andando :)

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => { res.render('index', { miTitulo: 'index me' }) });
+router.get('/', (req, res) => { res.render('index', { miTitulo: 'Home @falaca' }) });
 router.get('/about', (req, res) => { res.render('about', { miTitulo: 'About me' }) });
 router.get('/contact', (req, res) => { res.render('contact', { miTitulo: 'Contact me ' }) });
 
